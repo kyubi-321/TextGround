a41b34bed8d5e5449b216016a6be373be081db12
unknown <Ankitbadhani102@gmail.com>
2023-09-13 19:12:54 +0530

    I am commiting agin due to some changes

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -54,7 +54,7 @@ export default function TextForm(props) {
         </div>
         <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
             <h2>Text Summary</h2>
-            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters arepresent in this paragraph.</p>
+            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters are present in this paragraph.</p>
             <p>{0.008 *  text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Minutes will take to read this paragraph</p>
             <h2>Preview</h2>
             <p>{text.length>0?text:"Nothing to preview!"}</p>
